1de0a85261958ea7c3f49a443ab43cb586a0e47b
Yann THEVENIN <yann.thevenin@epitech.eu>
2023-02-11 14:47:51 +0100

    add: create board endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,24 @@
 import express from 'express'
 import cors from 'cors'
-import connectToDatabase from './database'
 
+import connectToDatabase from './database'
 import home from './endpoints/home'
-import register from './endpoints/register'
-import login from './endpoints/login'
+import register from './endpoints/door/register'
+import login from './endpoints/door/login'
+import create_board from './endpoints/board/create_board'
 
 const app = express()
 const port = 4000
 
 app.use(cors())
 app.use(express.json());
+
 connectToDatabase()
 
 app.get('/', home)
 app.post('/register', register)
 app.post('/login', login)
+app.post('/create_board', create_board)
 
 app.listen(port, () => {
     console.log(`App is listening on port: ${port} 🌵`)
