b325af6a1b2580fb183d96f2c1177d08d8cd8f7c
Joan THOMAS <thomas.joan.pl@gmail.com>
2023-02-11 12:19:09 +0100

    home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,68 @@
+import { ReactNode } from 'react';
+import {
+    Box,
+    Flex,
+    Link,
+    Button,
+    useColorModeValue,
+    Stack,
+    useColorMode,
+    Center,
+    Heading,
+} from '@chakra-ui/react';
 
-const Home = (): JSX.Element => (
-    <div>Home</div>
+const NavLink = ({ children }: { children: ReactNode }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+    </Link>
 );
+
+function Nav() {
+    const {toggleColorMode} = useColorMode();
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <Box>Turbo Task Manager</Box>
+
+                    <Flex alignItems={'center'}>
+                        <Stack direction={'row'} spacing={7}>
+                            <Button>
+                                Login
+                            </Button>
+                            <Button>
+                                Register
+                            </Button>
+                            <Button onClick={toggleColorMode}>
+                                Dark Mode
+                            </Button>
+                        </Stack>
+                    </Flex>
+                </Flex>
+            </Box>
+        </>
+    );
+}
+function Home() {
+    return (
+        <div>
+            <Nav></Nav>
+            <Center>
+                <Stack align='stretch'>
+                    <Heading>Turbo Task Manager</Heading>
+                    <Box justify-content={'align'}>Manage your turbo tasks</Box>
+                </Stack>
+            </Center>
+        </div>
+    );
+}
 
 export default Home;
